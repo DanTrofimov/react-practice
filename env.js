3737c2e798491ed7df7dd30eb76e7f810baefcb5
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-06-12 21:13:23 +0300

    Updates

---
+++ env.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZXhCCwtfiomKBHUgiOUMV9SiwcywTGMk",
+    authDomain: "my-app-bf4f3.firebaseapp.com",
+    projectId: "my-app-bf4f3",
+    storageBucket: "my-app-bf4f3.appspot.com",
+    messagingSenderId: "825377096146",
+    appId: "1:825377096146:web:b05f5da7f28f29eea0880b"
+};
